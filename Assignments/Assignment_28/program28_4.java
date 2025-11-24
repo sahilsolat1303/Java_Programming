d80a87ba38d1bcca05109b772709b530be94518c
sahilsolat1303 <sahilsolat19@gmail.com>
2025-11-24 08:38:11 +0530

    Add files via upload

---
+++ Assignments/Assignment_28/program28_4.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Pattern
+{
+    public void Display(int iRow,int iCal)
+    {
+        int i = 0;
+        int j = 0;
+        for(i=iRow;i>=1;i--)
+        {
+            for(j=1;j<=iCal;j++)
+            {
+                
+                     System.out.print(i+"\t");
+                
+            }
+            System.out.println();
+        }
+        
+        
+    }
+}
+
+class program28_4
+{
+    public static void main(String[] args) {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1,iValue2 = 0;
+
+        System.out.println("Enter the number of Row:");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of Column:");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1,iValue2);
+    }
+}
