830b2652aaa2ba574b82b0db851235c83ebd48c6
sahilsolat1303 <sahilsolat19@gmail.com>
2025-11-24 08:39:17 +0530

    Add files via upload

---
+++ Assignments/Assignment_29/program29_4.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Pattern
+{
+    public void Display(int iRow,int iCal)
+    {
+        int i =0;
+        int j =0;
+
+        for(i=1;i<=iRow;i++)
+            {
+                for(j=1;j<=iCal;j++)
+                {
+                    
+                    if(i%2!=1)
+                    {
+                        System.out.print((-j)+"\t");
+                       
+                    }
+                    else
+                    {
+                        System.out.print(j+"\t");
+                    }
+                }
+                System.out.println(); 
+            } 
+                 
+        
+    }
+}
+
+class program29_4
+{
+    public static void main(String[] args) {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1,iValue2 = 0;
+
+        System.out.println("Enter the number of Row:");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of Column:");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1,iValue2);
+    }
+}
---
+++ Assignments/Assignment_29/program29_5.java
@@ -0,0 +1,38 @@
+import java.util.*;
+class Pattern
+{
+    public void Display(int iRow,int iCal)
+    {
+        int i =0;
+        int j =0;
+
+        for(i=1;i<=iRow;i++)
+            {
+                for(j=1;j<=iCal;j++)
+                {
+                               
+                    System.out.print((i+j)-1+"\t");
+                }
+                System.out.println(); 
+            } 
+                 
+        
+    }
+}
+
+class program29_5
+{
+    public static void main(String[] args) {
+        Scanner sobj = new Scanner(System.in);
+        int iValue1,iValue2 = 0;
+
+        System.out.println("Enter the number of Row:");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of Column:");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern();
+        pobj.Display(iValue1,iValue2);
+    }
+}
